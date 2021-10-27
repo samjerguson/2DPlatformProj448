77bed9231e0eb19de61e2917bc186aeb175d2e89
blake richmeier <65925816+blakerichmeier@users.noreply.github.com>
2021-10-27 13:31:40 -0500

    Added Power Ups
    Currently the power ups have no functionality but will most likely be extra lives.

---
+++ 2DPlat448/Assets/Scripts/PowerUps.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUps : MonoBehaviour
+{
+    public GameObject pickupEffect;
+
+    void OnTriggerEnter2D(Collider2D other){
+      if(other.CompareTag("Player")){
+        PickUp();
+      }
+    }
+
+    void PickUp(){
+      Debug.Log("Picked up object");
+      gameObject.SetActive(false);
+    }
+}

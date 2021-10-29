e424e8d5c8dea11d54a69bd6d8fed3d906b8e062
PositiveFive <alecmcglynn@ku.edu>
2021-10-29 14:42:21 -0500

    Added game timer

---
+++ 2DPlat448/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+
+private float seconds = 0.0f;
+public Text disvar;
+
+void Update() 
+{              
+  seconds += Time.deltaTime;
+  int minutes = ((int)seconds) / 60; 
+  double b = System.Math.Round(seconds % 60, 2);
+  disvar.text = minutes.ToString("0") + ":" + b.ToString() + "s";      
+}
+}

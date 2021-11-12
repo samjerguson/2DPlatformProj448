a19e8b189430f72fa5c1274f21e4bd08a2783ce2
Ben Moeller <benlynnmoeller@gmail.com>
2021-11-11 20:37:09 -0600

    adding background music and code Patrick made

---
+++ 2DPlat448/Assets/Music/backgroundmusic.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class backgroundmusic : MonoBehaviour
+{
+
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "GameWon")
+        {
+            Destroy(this.gameObject);
+        }
+        if (SceneManager.GetActiveScene().name == "Menu")
+        {
+            Destroy(this.gameObject);
+        }
+        if (SceneManager.GetActiveScene().name == "GameOver")
+        {
+            Destroy(this.gameObject);
+        }
+        if (SceneManager.GetActiveScene().name == "TestSuite")
+        {
+            Destroy(this.gameObject);
+        }
+
+
+
+
+
+    }
+}
---
+++ 2DPlat448/Assets/Music/overmusic.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class overmusic : MonoBehaviour
+{
+
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "GameWon")
+        {
+            Destroy(this.gameObject);
+        }
+        if (SceneManager.GetActiveScene().name == "Menu")
+        {
+            Destroy(this.gameObject);
+        }
+        if (SceneManager.GetActiveScene().name == "SampleScene")
+        {
+            Destroy(this.gameObject);
+        }
+        if (SceneManager.GetActiveScene().name == "TestSuite")
+        {
+            Destroy(this.gameObject);
+        }
+
+
+
+
+
+    }
+}

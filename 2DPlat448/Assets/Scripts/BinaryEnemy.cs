191bddbad6a7d4807e739e1cb205c1c4291963cb
samjerguson <samjerguson@gmail.com>
2021-11-16 19:14:14 -0600

    Added binary rising obstacle

---
+++ 2DPlat448/Assets/Scripts/BinaryEnemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinaryEnemy : MonoBehaviour
+{
+    public float risingRate;
+    public float frameRate;
+    public Material mat;
+    public List<Texture> texture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TextureChange());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * risingRate * Time.deltaTime); //binary code block moves UP (I had to turn it upside down)       
+    }
+
+    IEnumerator TextureChange()
+    {
+        int i = 0;
+        while(true)
+        {
+            i++;
+            mat.mainTexture = texture[i % 21];
+            yield return new WaitForSeconds(frameRate);
+        }
+        yield return null;
+    }
+}
--- 2DPlat448/Assets/Scripts/Game.cs
+++ 2DPlat448/Assets/Scripts/Game.cs
@@ -141,5 +141,5 @@ public class Game : MonoBehaviour
                 }
             }
         //
-    }       
+    }
 }

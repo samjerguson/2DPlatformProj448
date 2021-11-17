0389320074d7b8eaf685a1c1521edb61efe2b9a1
samjerguson <samjerguson@gmail.com>
2021-11-16 19:56:20 -0600

    Binary Obstacle waits before moving now
    Wait time is a public variable

--- 2DPlat448/Assets/Scripts/BinaryEnemy.cs
+++ 2DPlat448/Assets/Scripts/BinaryEnemy.cs
@@ -4,35 +4,43 @@ using UnityEngine;
 
 public class BinaryEnemy : MonoBehaviour
 {
+    bool waiting = true;
     public float risingRate;
     public float frameRate;
+    public int waitTime;
     public Material mat;
     public List<Texture> texture;
     // Start is called before the first frame update
     void Start()
     {
-        if(MainMenu.isHard) {
-            StartCoroutine(TextureChange());
-        }
+        StartCoroutine(TextureChange());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(MainMenu.isHard) {
-            transform.Translate(Vector3.down * risingRate * Time.deltaTime); //binary code block moves UP (I had to turn it upside down)       
-        }
+        StartCoroutine(MoveBinary());
     }
 
     IEnumerator TextureChange()
     {
         int i = 0;
-        while(true)
+        while (true)
         {
             i++;
             mat.mainTexture = texture[i % 21];
             yield return new WaitForSeconds(frameRate);
         }
         yield return null;
+    }
+
+    IEnumerator MoveBinary()
+    {
+        if (waiting == true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            waiting = false;
+        }
+        transform.Translate(Vector3.down * risingRate * Time.deltaTime); //binary code block moves UP (I had to turn it upside down)
     }
 }
